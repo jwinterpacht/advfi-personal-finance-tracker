ac13c27f6864a40c12b191b35ce75ea779825897
JasonBTW23 <164063136+JasonBTW23@users.noreply.github.com>
2024-10-24 12:05:49 -0500

    Create IncomeReport.py
    Need to finish generate_report

---
+++ IncomeReport.py
@@ -0,0 +1,43 @@
+from datetime import date
+from typing import List
+
+class IncomeReport:
+    def __init__(self):
+        self._report_date: date = date.min  # Default date: 00/00/0000
+        self._total_income: float = 0.0     # Default total income is 0.0
+        self._income_entries: List[float] = []  # List to store income amounts (floats)
+
+  
+    #Getters
+    def get_report_date(self):
+        return self._report_date
+    
+    def get_income_entries(self):
+        return self._income_entries
+
+    def get_total_income(self):
+        return self._total_income
+
+  
+    #Setters
+    def set_report_date(self, report_date: date):
+        self._report_date = report_date
+
+    def set_income_entries(self, income_entries: List[float]):
+        self._income_entries = income_entries
+        self._total_income = sum(self._income_entries)
+
+    def set_total_income(self, total_income: float):
+        self._total_income = total_income
+
+
+    #Add an income entry
+    def add_income(self, amount: float):
+        self._income_entries.append(amount)
+        self._total_income += amount  # Update total income
+
+  
+    #Generates Report
+    def generate_report(self):
+       #Need to finish
+        return self
