b79c1a54256bd6228bcdfdd5647c949c2e7ecfc8
jwinterpacht <jadenwinterpacht@icloud.com>
2024-12-05 16:24:24 -0600

    Update SpendingReport.py
    Made class reflect the design outlined in the class diagram

--- SpendingReport.py
+++ SpendingReport.py
@@ -2,81 +2,21 @@ from datetime import date
 from typing import List, Dict
 
 class SpendingReport:
-    def __init__(self):
-        self.startDate = date.min 
-        self.endDate = date.min   
-        self.totalSpending = 0.0
-        self.expenses = [] 
+    def __init__(self):  
+        self._total_spending = 0.0
+        self._expenses = []
 
     # Getters
-    def getTotalSpending(self):
-        return self.totalSpending
+    def get_total_spending(self):
+        return self._total_spending
     
     def getExpenses(self):
-        return self.expenses
+        return self._expenses
 
 
     # Setters
-    def setTotalSpending(self, totalSpending: float):
-        self.totalSpending = totalSpending
+    def set_total_spending(self, total_spending: float):
+        self._total_spending = total_spending
     
-    def setExpenses(self, expenses: List[Dict]):
-        self.expenses = expenses
-
-
-
-    def addExpense(self, amount: float, expenseDate: date, category: str, isRecurring: bool = False):
-        expense = {
-            "amount": amount,
-            "expenseDate": expenseDate,
-            "category": category,
-            "isRecurring": isRecurring
-        }
-        self.expenses.append(expense)
-        self.totalSpending += amount
-    
-
-
-    def filterByDateRange(self, left: date, right: date):
-      
-        filteredExpenses = [i for i in self.expenses if left <= i["expenseDate"] <= right]
-        filteredReport = SpendingReport()
-        filteredReport.setExpenses(filteredExpenses)
-        filteredReport.setTotalSpending(sum(i["amount"] for i in filteredExpenses))
-      
-        return filteredReport
-
-    def filterByCategory(self, category: str):
-      
-        filteredExpenses = [i for i in self.expenses if i["category"] == category]
-        filteredReport = SpendingReport()
-        filteredReport.setExpenses(filteredExpenses)
-        filteredReport.setTotalSpending(sum(i["amount"] for i in filteredExpenses))
-      
-        return filteredReport
-
-    def filterByRecurring(self, isRecurring: bool = True):
-      
-        filteredExpenses = [i for i in self.expenses if i["isRecurring"] == isRecurring]
-        filteredReport = SpendingReport()
-        filteredReport.setExpenses(filteredExpenses)
-        filteredReport.setTotalSpending(sum(i["amount"] for i in filteredExpenses))
-      
-        return filteredReport
-
-
-
-    def generateReport(self):
-        reportSummary = {
-            "totalSpending": self.totalSpending,
-            "expenseCount": len(self.expenses),
-            "expensesByCategory": {}
-        }
-        for expense in self.expenses:
-            category = expense["category"]
-            if category in reportSummary["expensesByCategory"]:
-                reportSummary["expensesByCategory"][category] += expense["amount"]
-            else:
-                reportSummary["expensesByCategory"][category] = expense["amount"]
-
-        return reportSummary
+    def set_expenses(self, expenses):
+        self._expenses = expenses
