3a32b1e7d52a4c7fc1055fa356b52e51a2cd6db7
Mason Myre <82189404+Lancer077@users.noreply.github.com>
2024-12-08 21:43:07 -0600

    Update SpendingReport.py

--- SpendingReport.py
+++ SpendingReport.py
@@ -1,60 +1,60 @@
 from datetime import date
-from typing import List, Dict
+from typing import List
 import TransactionList
+import Transaction
 import MainUI
 from Report import Report
 
-class SpendingReport(Report):
+class IncomeReport(Report):
     def __init__(self, report_date: date, transaction_list: TransactionList):
         super().__init__(report_date)
-        self._total_spending_last_month = 0.0
-        self._expense_entries_last_month = []
-        for expense in transaction_list._expense_transactions:
-            if expense.get_is_within_last_month():
-                self._expense_entries_last_month.append(expense)
-                self._total_spending_last_month += expense.get_amount()
-        self._total_spending = transaction_list.get_total_expenses()
+        self._total_income_last_month = 0.0
+        self._income_entries_last_month = []  # List to store income amounts (floats)
+        for income in transaction_list._income_transactions:
+            if income.get_is_within_last_month():
+                self._income_entries_last_month.append(income)
+                self._total_income_last_month += income.get_amount()
+        self._total_income = transaction_list.get_total_income()  # Default total income is 0.0
 
-    # Getters
-    def get_expense_entries_last_month(self):
-        return self._expense_entries_last_month
+    # getters
+    def get_income_entries_last_month(self):
+        return self._income_entries_last_month
 
-    def get_total_spending(self):
-        return self._total_spending
+    def get_total_income(self):
+        return self._total_income
     
-    def get_total_spending_last_month(self):
-        return self._total_spending_last_month
+    def get_total_income_last_month(self):
+        return self._total_income_last_month
 
+    # setters
+    def set_income_entries_last_month(self, income_entries_last_month: List[float]):
+        self._income_entries_last_month = income_entries_last_month
+        self._total_income = sum(self.income_entries_last_month)
 
-    # Setters
-    def set_expense_entries_last_month(self, expense_entries_last_month: List[float]):
-        self._expense_entries_last_month = expense_entries_last_month
-        self._total_spending = sum(self.income_entries_last_month)
+    def set_total_income(self, total_income: float):
+        self._total_income = total_income
 
-    def set_total_spending(self, total_spending: float):
-        self._total_spending = total_spending
-    
-    def set_total_spending_last_month(self, total_spending_last_month: float):
-        self._total_spending_last_month = total_spending_last_month
+    def set_total_income_last_month(self, total_income_last_month: float):
+        self._total_income_last_month = total_income_last_month
 
     def generate_report(self):
-        spending_report = f"Spending Report\n{self._report_date}\n-----------------\nTotal lifetime spending: ${self._total_spending}\n\nTotal spending from last month: ${self._total_spending_last_month}\n\nEach spending entry from last month:\n"
-        for expense in self._expense_entries_last_month:
-            spending_report += f"{expense.print_transaction()}\n"
-        MainUI.MainUI.utility_print(spending_report)
+        income_report = f"Income Report\n{self._report_date}\n---------------\nTotal lifetime income: ${self._total_income}\n\nTotal income from last month: ${self._total_income_last_month}\n\nEach income entry from last month:\n"
+        for income in self._income_entries_last_month:
+            income_report += f"{income.print_transaction()}\n"
+        MainUI.MainUI.utility_print(income_report)
 
     def to_string(self) -> str:
-        spending_report = f"Spending Report\n{self._report_date}\n-----------------\nTotal lifetime spending: ${self._total_spending}\n\nTotal spending from last month: ${self._total_spending_last_month}\n\nEach spending entry from last month:\n"
-        for expense in self._expense_entries_last_month:
-            spending_report += f"{expense.print_transaction()}\n"
-        return spending_report
+        income_report = f"Income Report\n{self._report_date}\n---------------\nTotal lifetime income: ${self._total_income}\n\nTotal income from last month: ${self._total_income_last_month}\n\nEach income entry from last month:\n"
+        for income in self._income_entries_last_month:
+            income_report += f"{income.print_transaction()}\n"
+        return income_report
 
     def get_header(self) -> str:
-        header = f"Spending Report\n{self._report_date}"
+        header = f"Income Report\n{self._report_date}"
         return header
     
     def get_body(self) -> str:
-        body = f"Total lifetime spending: ${self._total_spending}\n\nTotal spending from last month: ${self._total_spending_last_month}\n\nEach spending entry from last month:\n"
-        for expense in self._expense_entries_last_month:
-            body += f"{expense.print_transaction()}\n"
+        body = f"Total lifetime income: ${self._total_income}\n\nTotal income from last month: ${self._total_income_last_month}\n\nEach income entry from last month:\n"        
+        for income in self._income_entries_last_month:
+            body += f"{income.print_transaction()}\n"
         return body
